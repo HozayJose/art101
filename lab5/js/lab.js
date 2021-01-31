7abe21b7b4e58f153420ed78ebf7dbd345120205
Jose Salas <jsalvadorsalas000@gmail.com>
2021-01-31 12:03:28 -0800

    more lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,9 @@
+
+
+/**textAnalysis = "Hello, my name is Jose Salas and I'm a 2nd year AGPM major student at UCSC";
+*console.log(textAnalysis);
+*document.writeln("<p> Bio: " + textAnalysis + "<br> <p>" );
+*textAnalysis+= "<br> I was in the STEM Magnet program throuhgout my middle school and high school years.";
+*console.log(textAnalysis);
+*document.writeln("<p> Bio: " + textAnalysis + "<br> <p>" );
+**/
