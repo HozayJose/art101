32885aece1c0043104e5e8baf762a96ce8be236d
Jose Salas <jsalvadorsalas000@gmail.com>
2021-03-16 23:36:49 -0700

    last section

--- Section D/js/ajax.js
+++ Section D/js/ajax.js
@@ -21,20 +21,41 @@ function getAjaxData() {
 // function errorFun(xhr, status, strErr) {
 //   console.log("Error"+strErr);
 
+//Hearthstone API
+// const settings = {
+// 	"async": true,
+// 	"crossDomain": true,
+// 	"url": "https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/Ysera",
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-key": "909f4c3445mshd86ad8d3cd0c6bfp1c3e7bjsn6848bc6d3df5",
+// 		"x-rapidapi-host": "omgvamp-hearthstone-v1.p.rapidapi.com"
+// 	}
+// };
+//
+// $.ajax(settings).done(function (response) {
+//   $("#ajax_text").append("<img src="+response[0].img+">");
+// 	console.log(response[0].img);
+// });
+
+//Genuis API
 const settings = {
 	"async": true,
 	"crossDomain": true,
-	"url": "https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/Ysera",
+	"url": "https://genius.p.rapidapi.com/artists/16775/songs",
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-key": "909f4c3445mshd86ad8d3cd0c6bfp1c3e7bjsn6848bc6d3df5",
-		"x-rapidapi-host": "omgvamp-hearthstone-v1.p.rapidapi.com"
+		"x-rapidapi-host": "genius.p.rapidapi.com"
 	}
 };
 
 $.ajax(settings).done(function (response) {
-  $("#ajax_text").append("<img src="+response[0].img+">");
-	console.log(response[0].img);
+	for (var i = 0; i < response.response.songs.length; i++) {
+		//console.log(response[i]);
+		$("#ajax_text").append("<img id='img_id_"+i+"' class='images' src="+response.response.songs[i].song_art_image_url+">");
+		console.log(response.response.songs[i].song_art_image_url);
+	}
 });
 
 }
